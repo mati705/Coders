f00489a7c212a92e8fc24371153efc451276dcb5
mati705 <matifaseeh@gmail.com>
2026-06-16 10:28:01 +0500

    .

---
+++ src/ArrayListStorage.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+// Stores all available courses. Easy add / remove / iterate.
+
+public class ArrayListStorage {
+    private ArrayList<Course> courses;
+
+    public ArrayListStorage() {
+        courses = new ArrayList<>();
+    }
+
+    public void addCourse(Course course) {
+        courses.add(course);
+    }
+
+    public Course getCourse(int index) {
+        return courses.get(index);
+    }
+
+    public Course findById(String courseId) {
+        for (Course c : courses) {
+            if (c.getCourseId().equals(courseId))
+                return c;
+        }
+        return null;
+    }
+
+    public ArrayList<Course> getAll() {
+        return courses;
+    }
+
+    public void displayCourses() {
+        for(Course c : courses) {
+            System.out.println(c);
+        }
+    }
+}
